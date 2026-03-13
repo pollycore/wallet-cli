dbc3c9580a3c94251445430b90582c9a40b96f19
Jorge Fonseca <jorgemjfonseca@users.noreply.github.com>
2026-03-13 14:27:14 +0000

    .

---
+++ AGENT.md
@@ -0,0 +1,1 @@
+Read the goal.yaml file
--- wallet/WALLET_WIFI.py
+++
@@ -1,6 +0,0 @@
-from WALLET_MESSENGER import WALLET_MESSENGER
-from PW import PW
-
-
-class WALLET_WIFI(WALLET_MESSENGER):
-    '''👱📎 https://quip.com/TOqaAYU731Bx/-Wallet-Wi-Fi'''
